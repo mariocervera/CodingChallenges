b93f8d8372171e598111b1d3dcc9479a752016e3
mariocervera <mariocervera@gmail.com>
2016-02-28 21:51:18 +0100

    Commit DisjointSets

---
+++ SortingAndSearching/DisjointSets.java
@@ -0,0 +1,61 @@
+package problemSolving;
+
+import java.util.Arrays;
+
+/****************************************************************************
+ * Problem statement:
+ * 
+ * Give an efficient algorithm to determine whether two arrays are disjoint.
+ * 
+ ****************************************************************************/
+
+public class DisjointSets {
+
+	/****************************************************************************
+	 * Solution:
+	 * 
+	 * We can sort one of the two input arrays, say A. Then, we can iterate the
+	 * other array, say B, and use binary search to determine whether the elements
+	 * of B appear in A. If N is the size of A and M the size of B, the complexity
+	 * of this algorithm is O( N+M log N). This is because we get O(N log N) for
+	 * the sorting step plus O(M log N) for the cost of iterating B and carry out
+	 * binary search in A. 
+	 * 
+	 ****************************************************************************/
+	
+	public static boolean areDisjoint(int[] a, int[] b) {
+		
+		// First, sort one of the arrays
+		
+		Arrays.sort(a);
+		
+		// Apply binary search to check disjointness in logarithmic time
+		
+		for(int i = 0; i < b.length; i++) {
+			
+			if(binarySearch(a, b[i], 0, a.length-1)) {
+				
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	private static boolean binarySearch(int[] a, int key, int low, int high) {
+		
+		if(low > high) return false;
+		
+		int middle = (low + high) / 2;
+		
+		if(a[middle] == key) return true;
+		
+		if(a[middle] > key) {
+			
+			return binarySearch(a, key, low, middle-1);
+		}
+		else {
+			return binarySearch(a, key, middle+1, high);
+		}
+	}
+}
