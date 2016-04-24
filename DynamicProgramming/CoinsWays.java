5bea7ae327543eef5e05b06668bbacfe59547343
mariocervera <mariocervera@gmail.com>
2016-04-25 00:13:15 +0200

    Improved format CoinsWays.java

--- DynamicProgramming/CoinsWays.java
+++ DynamicProgramming/CoinsWays.java
@@ -24,41 +24,41 @@ public class CoinsWays {
 	 * 
 	 * This problem can be solved applying dynamic programming. We store partial results in a bidimensional
 	 * matrix A. Each cell (m,n) of this matrix indicates the number of ways we can obtain the amount "n"
-	 * using only the first "m" coins. The result of the problem lies in A[M][N].
+	 * using only the first "m" coins. The solution to the problem lies in A[M][N].
 	 * 
 	 *****************************************************************************************************/
-	
+
 	public static long coinWays(int N, int[] C) {
-		
-        int M = C.length;
-        
-        // Matrix to store partial results
-        
-        long[][] A = new long[M+1][N+1];
-        
-        // Boundary conditions
-        
-        for(int m = 0; m <= M; m++) {
-            A[m][0] = 1;
-        }
-        
-        // Recurrence relation
-        // A[m][n]: The number of ways we can make change for "n" using only the first "m" coins is equal to:
-        //             - the number of ways we can make change for "n" using only the first "m-1" coins, plus ...
-        //             - the number of ways we can make change for "n-C[m]" using the first "m" coins.
-        
-        for(int m = 1; m <= M; m++) {
-            for(int n = 1; n <= N; n++) {
-            
-                if(n >= C[m-1]) {
-                    A[m][n] = A[m-1][n] + A[m][n-C[m-1]];
-                }
-                else {
-                    A[m][n] = A[m-1][n];
-                }
-            }
-        }
-        
-        return A[M][N];
+
+		int M = C.length;
+
+		// Matrix to store partial results
+
+		long[][] A = new long[M + 1][N + 1];
+
+		// Boundary conditions
+
+		for (int m = 0; m <= M; m++) {
+			A[m][0] = 1;
+		}
+
+		// Recurrence relation
+		//
+		// A[m][n]: The number of ways we can make change for "n" using only the first "m" coins is equal to:
+		//             - the number of ways we can make change for "n" using only the first "m-1" coins, plus ...
+		//             - the number of ways we can make change for "n-C[m]" using the first "m" coins.
+
+		for (int m = 1; m <= M; m++) {
+			for (int n = 1; n <= N; n++) {
+
+				if (n >= C[m - 1]) {
+					A[m][n] = A[m - 1][n] + A[m][n - C[m - 1]];
+				} else {
+					A[m][n] = A[m - 1][n];
+				}
+			}
+		}
+
+		return A[M][N];
 	}
 }
